de804a83f9723efe9b42a2ae7a6644e1b373637d
Jari Aalto <jari.aalto@cante.net>
2011-12-14 10:45:32 +0200

    bbdb-epkg-xactivate.el: Correct calls
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/bbdb-epkg-xactivate.el
+++ epackage/bbdb-epkg-xactivate.el
@@ -1,16 +1,15 @@
-(unless (featurep 'bbdb-install)
-  (require 'bbdb-install))
+(require 'bbdb-epkg-install)
 
 ;; Redundant, see bbdb-initialize in bbdb-install.el, but anyway...
-(when (fboundp 'bbdb-insinuate-gnus) ; Is bbdb-autoloads.el loaded?
-  (add-hook 'gnus-startup-hook  'bbdb-insinuate-gnus)
-  (add-hook 'mail-setup-hook    'bbdb-insinuate-sendmail)
-  (add-hook 'message-setup-hook 'bbdb-insinuate-message))
+(add-hook 'gnus-startup-hook  'bbdb-insinuate-gnus)
+(add-hook 'mail-setup-hook    'bbdb-insinuate-mail)
+(add-hook 'message-setup-hook 'bbdb-insinuate-message)
+(add-hook 'rmail-mode-hook    'bbdb-insinuate-rmail)
 
 ;; In BBDB, defined field 'mail-alias'
 ;; Requires that mail abbrevs are active
-(when (fboundp 'bbdb-define-all-aliases)
-  (add-hook 'mail-setup-hook    'bbdb-define-all-aliases)
-  (add-hook 'message-setup-hook 'bbdb-define-all-aliases))
+(when (fboundp 'bbdb-mail-aliases)
+  (add-hook 'mail-setup-hook    'bbdb-mail-aliases)
+  (add-hook 'message-setup-hook 'bbdb-mail-aliases))
 
 (provide 'bbdb-epkg-activate)
